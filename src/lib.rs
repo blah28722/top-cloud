7f4bc3a287b4dcee2c89f30bb4d5a7cbf78786a7
John Chen <john.hk.chen@gmail.com>
2023-12-26 16:39:50 -0800

    Create random facts generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,36 @@
+use std::str;
 use anyhow::Result;
 use spin_sdk::{
     http::{IntoResponse, Request, Method, Response},
     http_component,
 };
+use serde::{Serialize, Deserialize};
+use maud::html;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Fact {
+    fact: String,
+}
 
 #[http_component]
 async fn topcloud(_req: Request) -> Result<impl IntoResponse> {
-    // Create the outbound request object
     let req = Request::builder()
         .method(Method::Get)
         .uri("https://random-data-api.fermyon.app/animals/json")
         .build();
-
-    // Send the request and await the response
     let res: Response = spin_sdk::http::send(req).await?;
-
-    println!("{:?}", res);  // log the response
-    Ok(res)
+    let msg: Fact = serde_json::from_slice(res.body()).unwrap();
+    let markup = html! {
+        p {
+            "Hi, did you know that..."
+            br;
+            (msg.fact) "?" 
+        }
+        button { "What do I do?" }
+    };
+    Ok(Response::builder()
+        .status(200)
+        .header("content-type", "text/html; charset=utf-8")
+        .body(markup.into_string())
+        .build())
 }
